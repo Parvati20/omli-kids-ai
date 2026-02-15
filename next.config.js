5a00122a9bcdf98a1b224477ce501f08eb57438d
parvati awrul <parvati23@navgurukul.org>
2026-02-15 20:25:23 +0530

    added some code

--- next.config.js
+++ next.config.js
@@ -1,18 +1,42 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+// 	webpack(config) {
+// 		config.module.exprContextCritical = false;
+// 		config.module.unknownContextCritical = false;
+// 		config.module.wrappedContextCritical = false;
+// 		config.ignoreWarnings = [
+// 			...(config.ignoreWarnings || []),
+// 			{
+// 				module: /onnxruntime-web[\\/].*\.js$/,
+// 				message: /Critical dependency: require function is used in a way in which dependencies cannot be statically extracted/, 
+// 			},
+// 		];
+// 		return config;
+// 	},
+// };
+
+// module.exports = nextConfig;
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	webpack(config) {
-		config.module.exprContextCritical = false;
-		config.module.unknownContextCritical = false;
-		config.module.wrappedContextCritical = false;
-		config.ignoreWarnings = [
-			...(config.ignoreWarnings || []),
-			{
-				module: /onnxruntime-web[\\/].*\.js$/,
-				message: /Critical dependency: require function is used in a way in which dependencies cannot be statically extracted/, 
-			},
-		];
-		return config;
-	},
+  experimental: {
+    serverComponentsExternalPackages: ["speech-to-speech"],
+  },
+  webpack: (config) => {
+    config.experiments = {
+      asyncWebAssembly: true,
+    };
+
+    config.resolve.fallback = {
+      fs: false,
+      path: false,
+      crypto: false,
+    };
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
